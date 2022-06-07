0059e620fe8350de2052358ddf098c7193b5043e
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-06-07 10:48:58 -0700

    apple: revert back to original, apply first step to extract function

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -1,6 +1,20 @@
 # This code suffers from the "Long Function" smell.
 # To cure it, apply "extract function".
 
-def story
-  "Mr. Dursley was the director of a firm called Grunnings, which made drills.\n\n\n\n\nAnd that was it."
+def first_line
+  "Mr. Dursley was the director of a firm called Grunnings, which made drills."
 end
+
+def story
+  first = first_line
+  middle = ""
+  middle << "\n"
+  middle << "\n"
+  middle << "\n"
+  middle << "\n"
+  middle << "\n"
+  conclusion = "And that was it."
+  first + middle + conclusion
+end
+
+
