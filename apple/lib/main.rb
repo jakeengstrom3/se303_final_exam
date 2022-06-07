2a8d4ae19aef0a93c6217cabda1fd081b304f1ec
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-06-07 10:49:50 -0700

    apple: extract middle_line function

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -5,14 +5,18 @@ def first_line
   "Mr. Dursley was the director of a firm called Grunnings, which made drills."
 end
 
-def story
-  first = first_line
+def middle_line
   middle = ""
   middle << "\n"
   middle << "\n"
   middle << "\n"
   middle << "\n"
   middle << "\n"
+end
+
+def story
+  first = first_line
+  middle = middle_line
   conclusion = "And that was it."
   first + middle + conclusion
 end
