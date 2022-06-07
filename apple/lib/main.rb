c3e88d88915c9bd3eb2101f34688e68a7a3717ed
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-06-07 10:46:18 -0700

    remove temporary varaible 'first'

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -2,7 +2,5 @@
 # To cure it, apply "extract function".
 
 def story
-  first = "Mr. Dursley was the director of a firm called Grunnings, which made drills.\n\n\n\n\nAnd that was it."
-  
-  first
+  "Mr. Dursley was the director of a firm called Grunnings, which made drills.\n\n\n\n\nAnd that was it."
 end
